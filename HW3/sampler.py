f5b3f6d1c12bfe2ff6152b5b93114f8df7d8e3af
Parth Shah <pnshah@andrew.cmu.edu>
2024-03-12 19:45:59 -0400

    q1 hw3 done...need to teat

---
+++ HW3/sampler.py
@@ -0,0 +1,40 @@
+import math
+from typing import List
+
+import torch
+from ray_utils import RayBundle
+from pytorch3d.renderer.cameras import CamerasBase
+
+
+# Sampler which implements stratified (uniform) point sampling along rays
+class StratifiedRaysampler(torch.nn.Module):
+    def __init__(
+        self,
+        cfg
+    ):
+        super().__init__()
+
+        self.n_pts_per_ray = cfg.n_pts_per_ray
+        self.min_depth = cfg.min_depth
+        self.max_depth = cfg.max_depth
+
+    def forward(
+        self,
+        ray_bundle,
+    ):
+        # TODO (Q1.4): Compute z values for self.n_pts_per_ray points uniformly sampled between [near, far]
+        z_vals = torch.linspace(self.min_depth, self.max_depth, self.n_pts_per_ray).to(ray_bundle.origins.device)
+
+        # TODO (Q1.4): Sample points from z values
+        sample_points = ray_bundle.origins[..., None, :] + ray_bundle.directions[..., None, :] * z_vals[..., None]
+
+        # Return
+        return ray_bundle._replace(
+            sample_points=sample_points,
+            sample_lengths=z_vals * torch.ones_like(sample_points[..., :1]),
+        )
+
+
+sampler_dict = {
+    'stratified': StratifiedRaysampler
+}
